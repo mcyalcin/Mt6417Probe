503bfcf335998da1ec216d93362f958f949e1f8c
Mehmet Cihan Yalcin <mcyalcin@gmail.com>
2015-07-25 15:30:19 +0300

    optimize import

--- src/main/scala/com/mikrotasarim/ui/controller/FpgaController.scala
+++ src/main/scala/com/mikrotasarim/ui/controller/FpgaController.scala
@@ -2,7 +2,6 @@ package com.mikrotasarim.ui.controller
 
 import com.mikrotasarim.api.command.DeviceController
 import com.mikrotasarim.api.device.{ConsoleMockDeviceInterface, DeviceInterface, OpalKellyInterface}
-import com.mikrotasarim.ui.model.MemoryMap
 
 import scalafx.beans.property.BooleanProperty
 
